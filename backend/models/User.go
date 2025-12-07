7977b987bd6d43967ca60653ab5f798ce0544946
Mas Dzuky Ar-Rouf <dimasdzuky99@gmail.com>
2025-12-07 13:06:25 +0700

    clean

--- backend/models/User.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID       uint   `gorm:"primaryKey" json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"uniqueIndex"`
-	Password string `json:"-"`
-	Role     string `json:"role" gorm:"default:user"`
-}
